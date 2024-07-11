f259fabd6aeec31cb9ed1e98d5902954118f3472
HemanthGangula <hemanthgangula7@gmail.com>
2024-07-11 10:15:41 +0530

    add project

---
+++ src/RandomNumber.svelte
@@ -0,0 +1,10 @@
+<script>
+  let randomNumber = 0;
+
+  function generateRandomNumber() {
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+  }
+</script>
+
+<button on:click={generateRandomNumber}>Generate Random Number</button>
+<p>Random Number: {randomNumber}</p>
